ff7f155ffb1cb008c122307b87fd12abb21ba57e
Ankur811201 <164860919+Ankur811201@users.noreply.github.com>
2026-08-06 11:51:12 +0530

    ry

--- routes/githubWebhook.js
+++ routes/githubWebhook.js
@@ -7,26 +7,18 @@ const router = express.Router();
 
 
 router.post("/api/github-webhook", express.raw({ type: "*/*" }), (req, res) => {
-    const secret = process.env.GITHUB_WEBHOOK_SECRET;
+    try {
+        console.log("Webhook received");
 
-    const signature = req.headers["x-hub-signature-256"];
-    const expected =
-        "sha256=" +
-        crypto.createHmac("sha256", secret).update(req.body).digest("hex");
+        // your code
 
-    if (signature !== expected) {
-        return res.status(401).send("Invalid signature");
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({
+            message: e.message,
+            stack: e.stack
+        });
     }
-
-    exec(
-        "cd /home/tradingn/tradingapp/nove-trade && git pull origin main && npm install",
-        (err, stdout, stderr) => {
-            console.log(stdout);
-            console.log(stderr);
-        }
-    );
-
-    res.send("Deployment started");
 });
 
 module.exports = router;
